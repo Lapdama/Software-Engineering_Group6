f1761e09c92352c96875c2385ce0cb20cf918998
david-oberko <david.oberko@st.vvu.edu.gh>
2017-04-10 16:37:36 -0700

    Add files via upload

---
+++ PReport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEProject
+{
+    public partial class PReport : Form
+    {
+        public PReport()
+        {
+            InitializeComponent();
+
+            // Create an instance of Datatable and the Report
+            DataTable oba = new DataTable();
+            CRPurchases rpt = new CRPurchases();
+
+            //An instance of the table adapter to retrieve the records
+            ProjectTableAdapters.PurchasesTableAdapter ob = new ProjectTableAdapters.PurchasesTableAdapter();
+
+            oba = ob.GetData();
+
+            rpt.SetDataSource(oba);
+
+            crystalReportViewer1.ReportSource = rpt;
+
+        }
+    }
+}
---
+++ SReport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEProject
+{
+    public partial class SReport : Form
+    {
+        public SReport()
+        {
+            InitializeComponent();
+
+            // Create an instance of Datatable and the Report
+            DataTable oba = new DataTable();
+            CRSales rpt = new CRSales();
+
+            //An instance of the table adapter to retrieve the records
+            ProjectTableAdapters.SalesTableAdapter ob = new ProjectTableAdapters.SalesTableAdapter();
+
+            oba = ob.GetData();
+
+            rpt.SetDataSource(oba);
+
+            crystalReportViewer1.ReportSource = rpt;
+        }
+    }
+}
